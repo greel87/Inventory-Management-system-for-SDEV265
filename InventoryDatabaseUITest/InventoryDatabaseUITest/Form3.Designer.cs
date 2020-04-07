901ddd71046a0aa43703f4969c7c427d72d01d02
Paul Stybert <paul.stybert@gmail.com>
2020-04-07 12:05:46 -0400

    Changed Form3 Add Customer form back to email and phone on top

--- InventoryDatabaseUITest/InventoryDatabaseUITest/Form3.Designer.cs
+++ InventoryDatabaseUITest/InventoryDatabaseUITest/Form3.Designer.cs
@@ -159,7 +159,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(444, 229);
+            this.label5.Location = new System.Drawing.Point(450, 143);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(45, 18);
             this.label5.TabIndex = 22;
@@ -169,7 +169,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(593, 229);
+            this.label6.Location = new System.Drawing.Point(593, 143);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(51, 18);
             this.label6.TabIndex = 23;
@@ -179,7 +179,7 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(417, 143);
+            this.label7.Location = new System.Drawing.Point(417, 229);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(105, 18);
             this.label7.TabIndex = 24;
@@ -189,7 +189,7 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(611, 134);
+            this.label8.Location = new System.Drawing.Point(611, 229);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(33, 18);
             this.label8.TabIndex = 25;
