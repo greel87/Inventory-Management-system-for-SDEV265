2fcb53c38326059c36224b67dbd58b6071882668
Paul Stybert <paul.stybert@gmail.com>
2020-04-15 00:45:14 -0400

    Added date column to orders and nullable serial number to stock

--- Documentation/Inventory_Management_Create.sql
+++ Documentation/Inventory_Management_Create.sql
@@ -11,13 +11,16 @@ Zip char(5) not null,
 Phone char(10) not null,
 Email varchar(255)
 );
+
 Create Table if not exists Stock
 (
 UPC varchar(12)  PRIMARY KEY,
 Prod_Name varchar(255) Not null,
 Serial_Number_Required boolean Not null,
-Quantity int unsigned not null
+Quantity int unsigned not null,
+Serial_number int
 );
+
 Create Table if not exists SFS
 (
 Serial_Number varchar(255) PRIMARY KEY,
@@ -27,6 +30,7 @@ Warranty boolean,
 
 FOREIGN KEY (UPC) REFERENCES Stock(UPC)
 );
+
 Create Table if not exists F3000
 (
 Serial_Number varchar(255) PRIMARY KEY,
@@ -36,13 +40,16 @@ Warranty boolean,
 
 FOREIGN KEY (UPC) REFERENCES Stock(UPC)
 );
+
 Create Table if not exists Orders
 (
 Order_ID int UNSIGNED not null  AUTO_INCREMENT PRIMARY KEY,
 Customer_ID INT UNSIGNED not null,
+Order_Date Date not null
 
 FOREIGN KEY (Customer_ID) REFERENCES customer(Customer_ID)
 );
+
 Create Table if not exists Order_Line
 (
 Order_ID int UNSIGNED not null,
