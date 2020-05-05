b801edf92ffc38a98cf5645f14ebf2c53f5b67d2
Liz Hall <elizabethvhall@gmail.com>
2020-05-05 18:38:09 -0400

    Update UI
    Forgot to push on Sunday

--- InventoryDatabaseUITest/InventoryDatabaseUITest/ViewInventoryfrm.Designer.cs
+++ InventoryDatabaseUITest/InventoryDatabaseUITest/ViewInventoryfrm.Designer.cs
@@ -28,48 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.invSearchbtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.prodtxtbx = new System.Windows.Forms.TextBox();
             this.backbtn = new System.Windows.Forms.Button();
             this.prodNametxtbx = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnViewinventory = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
             this.stockDatagridVw = new System.Windows.Forms.DataGridView();
             this.print = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.stockDatagridVw)).BeginInit();
             this.SuspendLayout();
-            // 
-            // invSearchbtn
-            // 
-            this.invSearchbtn.Location = new System.Drawing.Point(227, 128);
-            this.invSearchbtn.Name = "invSearchbtn";
-            this.invSearchbtn.Size = new System.Drawing.Size(75, 23);
-            this.invSearchbtn.TabIndex = 1;
-            this.invSearchbtn.Text = "Search";
-            this.invSearchbtn.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 133);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "UPC";
-            // 
-            // prodtxtbx
-            // 
-            this.prodtxtbx.Location = new System.Drawing.Point(105, 128);
-            this.prodtxtbx.Name = "prodtxtbx";
-            this.prodtxtbx.Size = new System.Drawing.Size(116, 20);
-            this.prodtxtbx.TabIndex = 3;
             // 
             // backbtn
             // 
@@ -83,7 +49,7 @@
             // 
             // prodNametxtbx
             // 
-            this.prodNametxtbx.Location = new System.Drawing.Point(105, 169);
+            this.prodNametxtbx.Location = new System.Drawing.Point(101, 190);
             this.prodNametxtbx.Name = "prodNametxtbx";
             this.prodNametxtbx.Size = new System.Drawing.Size(116, 20);
             this.prodNametxtbx.TabIndex = 7;
@@ -91,7 +57,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(24, 174);
+            this.label2.Location = new System.Drawing.Point(20, 195);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 6;
@@ -99,63 +65,13 @@
             // 
             // btnViewinventory
             // 
-            this.btnViewinventory.Location = new System.Drawing.Point(227, 169);
+            this.btnViewinventory.Location = new System.Drawing.Point(223, 190);
             this.btnViewinventory.Name = "btnViewinventory";
             this.btnViewinventory.Size = new System.Drawing.Size(75, 23);
             this.btnViewinventory.TabIndex = 5;
             this.btnViewinventory.Text = "Search";
             this.btnViewinventory.UseVisualStyleBackColor = true;
             this.btnViewinventory.Click += new System.EventHandler(this.btnViewinventory_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(105, 206);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(116, 20);
-            this.textBox2.TabIndex = 10;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 211);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Serial Number";
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(227, 206);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 8;
-            this.button3.Text = "Search";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(105, 242);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(116, 20);
-            this.textBox3.TabIndex = 13;
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 247);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 13);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "Quantity";
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(227, 242);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "Search";
-            this.button4.UseVisualStyleBackColor = true;
             // 
             // stockDatagridVw
             // 
@@ -182,19 +98,10 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.print);
             this.Controls.Add(this.stockDatagridVw);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button3);
             this.Controls.Add(this.prodNametxtbx);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.btnViewinventory);
             this.Controls.Add(this.backbtn);
-            this.Controls.Add(this.prodtxtbx);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.invSearchbtn);
             this.Name = "ViewInventoryfrm";
             this.Text = "Search Inventory";
             ((System.ComponentModel.ISupportInitialize)(this.stockDatagridVw)).EndInit();
@@ -204,19 +111,10 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button invSearchbtn;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox prodtxtbx;
         private System.Windows.Forms.Button backbtn;
         private System.Windows.Forms.TextBox prodNametxtbx;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnViewinventory;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button4;
         private System.Windows.Forms.DataGridView stockDatagridVw;
         private System.Windows.Forms.Button print;
     }
